8a759a1bdb67da65466386469befaa34bbac66b8
Nicolas Epstein <nilueps@gmail.com>
2021-03-31 19:04:27 -0400

    layout fixes, minor header change

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import posts from "assets/posts/*.md";
 
-// console.log(md);
-
 const blog = document.querySelector("#blog-section")
 
 for (let post in posts) {
-    console.log(post, posts[post])
     const article = document.createElement("article");
     article.innerHTML = posts[post];
     blog.appendChild(article);
