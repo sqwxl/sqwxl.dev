b27b7882e54fc2cffc424c56ed4197c0bd90b306
NIcolas Epstein <nilueps@gmail.com>
2021-05-01 09:38:31 -0400

    typo, layout fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import posts from "./blog/posts/*.md";
 
-const blog = document.querySelector("#blog");
+const blog = document.querySelector("#blog--posts");
 
 for (let post in posts) {
   const article = document.createElement("article");
