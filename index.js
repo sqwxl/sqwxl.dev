1bccc065bde5fee5793c39d68fdc646311ac3f78
NIcolas Epstein <nilueps@gmail.com>
2021-05-01 16:59:22 -0400

    zola

--- index.js
+++
@@ -1,9 +0,0 @@
-import posts from "./blog/posts/*.md";
-
-const blog = document.querySelector("#blog--posts");
-
-for (let post in posts) {
-  const article = document.createElement("article");
-  article.innerHTML = posts[post];
-  blog.insertBefore(article, blog.childNodes[0]);
-}
